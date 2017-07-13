cc1cf4723011acb124ce885f5f006c28843803af
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-07-12 20:55:17 -0400

    finish assignment

--- server.rb
+++ server.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
 end
 
 get '/portfolio' do
@@ -9,5 +17,12 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about
+end
+
+get '/favorites' do
+  @fav_links = ['<a href="http://www.google.com">Google</a>', '<a href="http://www.youtube.com">Youtube</a>', '<a href="http://www.reddit.com">Reddit</a>']
+  erb :favorites
 end
